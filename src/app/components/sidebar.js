e74540c390bc24847420bf3004c1ab77d4c3b7f0
bagusperdanay7 <baguspyus@gmail.com>
2024-07-11 15:52:12 +0700

    feat: card home page

--- src/app/components/sidebar.js
+++ src/app/components/sidebar.js
@@ -1,5 +1,9 @@
 import Link from "next/link";
 
 export default function Sidebar() {
-  return <div>Sidebar</div>;
+  return (
+    <nav className="mb-6 md:mb-0">
+      <div>Logo</div>
+    </nav>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import daisyui from "daisyui";
+
 module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -31,5 +33,5 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [daisyui],
 };
