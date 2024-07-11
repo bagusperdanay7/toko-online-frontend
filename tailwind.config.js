7c4801a14c9f1251c5b7ee412355be8d23abdb3b
Bagus Perdana Yusuf <baguspyus@gmail.com>
2024-07-11 13:53:41 +0700

    first upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,23 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        "primary-light-bg": "#F9F8FB",
+        "secondary-light-bg": "#FFFFFF",
+        coffee: {
+          50: "#faf7f0",
+          100: "#efe7d6",
+          200: "#decea9",
+          300: "#cdb27c",
+          400: "#c19c5e",
+          500: "#b6834a",
+          600: "#a0693f",
+          700: "#865137",
+          800: "#704332",
+          900: "#5c382b",
+          950: "#331c15",
+        },
+      },
     },
   },
   plugins: [],
